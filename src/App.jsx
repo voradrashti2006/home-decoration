ecc21b31ef1b9186d3f6e06406b47d4cf331a827
Pravinmali123 <pravinmali24899@gmail.com>
2026-01-19 19:25:42 +0530

    home commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Header from './pages/Header'
+import Hero from './pages/Hero'
+import Product from './pages/Product';
+import Fotter from './pages/Fotter';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+
+function App() {
+  return (
+    <>
+      {/* <Header></Header> */}
+      {/* <Hero></Hero> */}
+      {/* <Product></Product> */}
+      {/* <Fotter></Fotter> */}
+      {/* <About></About> */}
+     <Portfolio></Portfolio>
+
+    </>
+  );
+}
+
+export default App;
+
+
